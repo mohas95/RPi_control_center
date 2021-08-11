33eca6569fa228f18cab6e10e3c360494608d79e
moha7108 <moha7108@pm.me>
2021-08-10 21:22:09 -0400

    integrate relay classes for state handling

--- relay-control.py
+++ relay-control.py
@@ -1,5 +1,5 @@
 import sys
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import threading
 import time
 import datetime
@@ -7,14 +7,17 @@ import json
 import os.path
 import glob
 
+import relay_states as rs
+from relay_states import Relay
+
 #################### global variable
 relay_config_file = './relay_config.json'
 
 
-
-Relay_Ch1 = 26
-Relay_Ch2 = 20
-Relay_Ch3 = 21
+#
+# Relay_Ch1 = 26
+# Relay_Ch2 = 20
+# Relay_Ch3 = 21
 
 
 
@@ -22,19 +25,12 @@ Relay_Ch3 = 21
 
 # def set_relay(relay, state = False):
 
-def load_config_json(config_file):
-
-    if os.path.isfile(config_file):
-
-        with open(config_file, "r") as f:
-            result = json.load(f)
-
-        return result
-
-    else:
-        return None
-
 if __name__ == '__main__':
 
-    if os.path.isfile(relay_config_file):
-        load_config_json
+    relay_config = rs.load_relay_config(relay_config_file)
+
+    relays= rs.load_relay_objects(relay_config)
+
+    # relays['1'].name = 'air_pump'
+
+    print(relays)
---
+++ relay_states.py
@@ -0,0 +1,95 @@
+import os.path
+import json
+import threading
+
+def load_relay_config(config_json_file):
+    '''
+    return results: dictionary
+    '''
+    default_relay_config = {
+        1:{'name':None, 'pin':26, 'state':None},
+        2:{'name':None, 'pin':20, 'state':None},
+        3:{'name':None, 'pin':21, 'state':None},
+    }
+
+    if os.path.isfile(config_json_file):
+
+        with open(config_json_file, "r") as f:
+            result = json.load(f)
+
+    else:
+        result = default_relay_config
+        with open(config_json_file, "w") as f:
+            f.write(json.dumps(result, indent=4))
+
+    return result
+
+
+def load_relay_objects(relay_config):
+
+    relay_objects = {}
+
+    for relay_id, relay_properties in relay_config.items():
+
+        relay = Relay(id = relay_id,name = relay_properties['name'], pin=relay_properties['pin'])
+
+        relay_objects[relay_properties['name']] = relay
+
+    return relay_objects
+
+
+
+class Relay():
+    def __init__(self, id, name, pin):
+        self.id = id
+        self.name = name
+        self.pin = pin
+        self.state = False
+
+    @property
+    def id(self):
+        '''
+        '''
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        '''
+        '''
+        self._id = value
+
+    @property
+    def name(self):
+        '''
+        '''
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        '''
+        '''
+        self._name = value
+
+    @property
+    def pin(self):
+        '''
+        '''
+        return self._pin
+
+    @pin.setter
+    def pin(self, value):
+        '''
+        '''
+        self._pin = value
+
+    @property
+    def state(self):
+        '''
+        '''
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        '''
+        '''
+        self._state = value
