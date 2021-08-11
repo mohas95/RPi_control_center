6bc8f0edee534cdfa2af26079b1a7d433c0f2ddd
moha7108 <moha7108@pm.me>
2021-08-11 01:55:05 -0400

    except to cleanup gpio pin upon exit

--- relay-control.py
+++ relay-control.py
@@ -1,6 +1,4 @@
 import sys
-# import RPi.GPIO as GPIO
-import threading
 import time
 import datetime
 import json
@@ -21,8 +19,10 @@ if __name__ == '__main__':
     relay_config = rs.load_relay_config(relay_config_file)
     relays= rs.load_relay_objects(relay_config)
 
-    while True:
-        rs.update_relay_states(dict_of_relays = relays, relay_config_file=relay_config_file)
-        rs.print_all_relays(relays)
-        time.sleep(refresh_rate)
-    rs.stop()
+    try:
+        while True:
+            rs.update_relay_states(dict_of_relays = relays, relay_config_file=relay_config_file)
+            rs.print_all_relays(relays)
+            time.sleep(refresh_rate)
+    except:
+        rs.stop()
