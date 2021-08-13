507a78a6a6034328d3d946b546a26755cc950287
moha7108 <moha7108@pm.me>
2021-08-13 18:34:04 -0400

    add

--- relay_states.py
+++ relay_states.py
@@ -11,17 +11,21 @@ import logzero
 from logzero import logger, setup_logger
 
 ########################################################### Global Variables
+# Default configuration for a 3 channel relay, this can be modified for whatever you want the default state to be
 default_relay_config = {
         "1":{'name':'name1', 'pin':26, 'state':False},
         "2":{'name':'name2', 'pin':20, 'state':False},
         "3":{'name':'name3', 'pin':21, 'state':False},
 }
 
-format = '%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(funcName)s %(thread)d]%(end_color)s %(message)s'
-formatter = logzero.LogFormatter(fmt=format)
+format = '%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(funcName)s %(thread)d]%(end_color)s %(message)s' # format for the logzero logger
+formatter = logzero.LogFormatter(fmt=format) # format object for logzero logger
 
 ########################################################### Wrapper/decorator definition function
 def threaded(func):
+    '''
+
+    '''
     def wrapper(*args, **kwargs):
 
         thread = threading.Thread(target=func, args=args, kwargs=kwargs, daemon=False)
@@ -31,9 +35,60 @@ def threaded(func):
     return wrapper
 
 ########################################################### Classes
-### Relay Class
+##################### Relay Class
 class Relay():
+    """
+    A class to represent a relay. Supports non-blocking multi-process threading,
+    for near-realtime control of individual relays
+
+    Attributes
+    ----------
+    id : str
+        Relay id
+    name : str
+        Name of the relay
+    pin : int
+        Pin attributed to relay
+    state : bool
+        State of the relay (ON or OFF)
+    refresh_rate : int
+        Refresh rate of state check
+    api_file : str
+        location of the api_file
+    logger : logging.logger
+        logger object
+    thread : threading.thread
+        thread object for parallel processing
+
+    Methods
+    -------
+    push_to_api(custom_api_file = None):
+        Prints the person's name and age.
+
+    @threaded
+    def start():
+        Prints the person's name and age.
+    """
+
     def __init__(self, id, name, pin, state=False, refresh_rate = 1, log_file = './logs/process.log'):
+        """
+        Constructs all the necessary attributes for the Relay object.
+
+        Parameters
+        ----------
+            id : str
+                Relay id
+            name : str
+                Name of the relay
+            pin : int
+                Pin attributed to relay
+            state : int
+                State of the relay (ON or OFF)
+            refresh_rate : int
+                Refresh rate of state check
+            log_file : str
+                location of logfile
+        """
         self.id = id
         self.name = name
         self.pin = pin
@@ -45,94 +100,90 @@ class Relay():
 
     @property
     def id(self):
-        '''
-        '''
+        """Return the id of the relay."""
         return self._id
 
     @id.setter
     def id(self, value):
-        '''
-        '''
-        self._id = value
+        """Set the id string of the relay."""
+        self._id = str(value)
 
     @property
     def name(self):
-        '''
-        '''
+        """Return the name of the relay."""
         return self._name
 
     @name.setter
     def name(self, value):
-        '''
-        '''
-        self._name = value
+        """Set the name string of the relay."""
+        self._name = str(value)
 
     @property
     def pin(self):
-        '''
-        '''
+        """Return the pin of the relay."""
         return self._pin
 
     @pin.setter
     def pin(self, value):
-        '''
-        '''
+        """Set the pin of the relay."""
+        if not isinstance(value, int):
+            raise TypeError("Pin must be an integer value")
         self._pin = value
 
     @property
     def state(self):
-        '''
-        '''
+        """Return the state of the relay."""
         return self._state
 
     @state.setter
     def state(self, value):
-        '''
-        '''
+        """Set the state of the relay."""
+        if not isinstance(value, bool):
+            raise TypeError("State can only be set to a bool variable")
         self._state = value
 
     @property
     def refresh_rate(self):
-        '''
-        '''
+        """Return the refresh rate of the relay."""
         return self._refresh_rate
 
     @refresh_rate.setter
     def refresh_rate(self, value):
-        '''
-        '''
+        """Set the refresh rate of the relay."""
+        if not isinstance(value, int):
+            raise TypeError("refresh rate must be an integer value")
         self._refresh_rate = value
 
     @property
-    def api_file(self):
-        '''
-        '''
-        return self._api_file
-
-    @property
     def logger(self):
-        '''
-        '''
+        """Return the logger object file location of the relay."""
         return self._logger
 
     @logger.setter
     def logger(self, value):
-        '''
-        '''
+        """Set the logger of the relay."""
+        if not instance(value, logging.logger):
+            raise TypeError("refresh rate must be an integer value")
         self._logger = value
+
+    @property
+    def api_file(self):
+        """Return the API file location of the relay."""
+        return self._api_file
 
     @api_file.setter
     def api_file(self, value):
-        '''
-        '''
+        """Set the API file location of the relay."""
+        if not isinstance(value, str):
+            raise TypeError("API file must be a string")
         self._api_file = value
 
-    def push_to_api(self, value = None):
-        if value:
-            self.api_file = value
+    def push_to_api(self, custom_api_file = None):
+        """Takes the api file attribute or custum api_file and pushes the state and attributes of the relay to the file"""
+        if custom_api_file:
+            self.api_file = custom_api_file
         else:
             self.api_file = './api/relay'+self.id +'_'+str(self.pin)+ '.json'
-
         timestamp = datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
         data = {"relay_id":self.id, "name":self.name,"pin":self.pin, "status":self.state, "last updated":timestamp}
 
@@ -141,6 +192,7 @@ class Relay():
 
     @threaded
     def start(self):
+        """Starts the non-blocking parallel relay thread controlling physical state of the relay based on its attributes"""
         success = False
         while self.state:
             try:
@@ -149,7 +201,6 @@ class Relay():
                     GPIO.setup(pin, GPIO.OUT)
                     GPIO.output(pin, GPIO.HIGH)
                     self.logger.info(f'[{self.id}:{self.name}]GPIO {pin} sucessfull initialized')
-
                     success = True
             except:
                 self.logger.error(f'[{self.id}:{self.name}]GPIO {pin} failed to initialize')
@@ -159,24 +210,19 @@ class Relay():
                     if GPIO.input(pin):
                         GPIO.output(pin, GPIO.LOW)
                         self.logger.info(f'[{self.id}:{self.name}]GPIO {pin} Switch ON')
-
                     else:
                         pass
-
                     time.sleep(self.refresh_rate)
             except:
                 GPIO.output(pin, GPIO.HIGH)
                 GPIO.cleanup(pin)
-
                 self.logger.error(f'[{self.id}:{self.name}]GPIO {pin} Error with the process, switching OFF and cleaning pin')
                 exit()
-
-
             GPIO.output(pin, GPIO.HIGH)
             GPIO.cleanup(pin)
             self.logger.info(f'[{self.id}:{self.name}]GPIO {pin} Switch OFF')
 
-### BulkUpdater Class
+##################### BulkUpdater Class
 class BulkUpdater():
     def __init__(self,config_file, default_config = default_relay_config , refresh_rate = 1, log_file = './logs/system.log'):
         self.status = False
@@ -329,7 +375,6 @@ class BulkUpdater():
             print('Major Error relay objects could not be loaded')
             exit()
 
-
     def update_relay_states(self):
         '''
         '''
@@ -444,9 +489,10 @@ if __name__ == '__main__':
     control_box = BulkUpdater(config_file = './relay_config.json', default_config = default_relay_config , refresh_rate = 1)
     control_box.start()
 
+
+    ######### You can put any code because this function is non-blocking
     try:
         while True:
-            pass
-            # time.sleep(5)
+            time.sleep(5)
     except:
         control_box.stop()
