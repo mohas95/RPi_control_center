369422696fc96303fbdfb5bdd478b032fc99715a
moha7108 <moha7108@pm.me>
2021-08-14 20:25:13 -0400

    add test file

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 from rpi_control_center_pkg import state_updater as su
 
-control_box = su.BulkUpdater(config_file = './relay_config.json', default_config = default_relay_config , refresh_rate = 1)
-control_box.start()
 
-######### You can put any code because this function is non-blocking
-try:
-    while True:
-        time.sleep(5)
-except:
-    control_box.stop()
+
+
+
+if __name__ == '__main__':
+    control_box = su.BulkUpdater(config_file = './relay_config.json', default_config = default_relay_config , refresh_rate = 1)
+    control_box.start()
+
+    ######### You can put any code because this function is non-blocking
+    try:
+        while True:
+            time.sleep(5)
+    except:
+        control_box.stop()
