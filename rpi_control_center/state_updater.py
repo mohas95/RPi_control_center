96dac3d13de26f51e698a7eb53f20de56e228f00
moha7108 <moha7108@pm.me>
2021-08-16 11:12:49 -0400

    add the possibilty of debug mode

--- rpi_control_center/state_updater.py
+++ rpi_control_center/state_updater.py
@@ -20,6 +20,7 @@ default_relay_config = {
 
 format = '%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(funcName)s %(thread)d]%(end_color)s %(message)s' # format for the logzero logger
 formatter = logzero.LogFormatter(fmt=format) # format object for logzero logger
+debug_mode = False
 
 ########################################################### Wrapper/decorator definition function
 def threaded(func):
@@ -99,7 +100,7 @@ class Relay():
         self.state = state
         self.refresh_rate = refresh_rate
         self.api_file = api_dir+'relay'+self.id +'_'+str(self.pin)+'.json'
-        self.logger = setup_logger(name=str(__name__)+"_process_logger", logfile=log_file, level=20, formatter = formatter, maxBytes=2e6, backupCount=3)
+        self.logger = setup_logger(name=str(__name__)+"_process_logger", logfile=log_file, level=10 if debug_mode else 20, formatter = formatter, maxBytes=2e6, backupCount=3)
         self.thread = self.start()
 
     @property
@@ -300,7 +301,7 @@ class BulkUpdater():
         self.saved_config = {}
         self.refresh_rate = refresh_rate
         self.log_dir = log_dir
-        self.logger = setup_logger(name=str(__name__)+"_status_logger", logfile=log_file, level=20, formatter = formatter, maxBytes=2e6, backupCount=3)
+        self.logger = setup_logger(name=str(__name__)+"_status_logger", logfile=log_file, level=10 if debug_mode else 20, formatter = formatter, maxBytes=2e6, backupCount=3)
         self.relay_dict = self.load_relay_objects()
         self.thread = None
 
