bf8d346c9dc0fd0028a2b723a353b9afe23f664b
moha7108 <moha7108@pm.me>
2021-08-16 15:35:19 -0400

    update test script to reflect changes

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ default_relay_config = {
 }
 
 if __name__ == '__main__':
-    control_box = GPIO_engine.BulkUpdater(config_file = './relay_config.json', refresh_rate = 1)
+    control_box = GPIO_engine.BulkUpdater(config_file = './relay_config.json',default_config =default_relay_config , refresh_rate = 1)
     control_box.start()
 
     ######### You can put any code because this function is non-blocking
